14cfc5e9750db0a480142b6c0feddd96260758b5
Moisés Cervantes <cervantes.moises@me.com>
2016-01-21 13:39:02 -0600

    Primer Commit

---
+++ AGoodStart/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Moisés Cervantes on 13/12/15.
+//  Copyright © 2015 Moisés Cervantes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    //Print the begining of the novel
+    
+    printf("It was the best of times. \n");
+    printf("It was the worst of times. \n");
+    
+    /* Is that actually any good?
+       Maybe it needs to rewrite */
+    
+    return 0;
+    
+}
